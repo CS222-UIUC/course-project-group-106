15ca1d5b6a24f5d2f8cf0b5f9deda4254423b05d
palakkotwani <kotwani3@illinois.edu>
2022-10-14 17:43:26 -0500

    Plotted Histogram

--- src/budget_tracker.py
+++ src/budget_tracker.py
@@ -3,7 +3,10 @@
 # pylint: disable=duplicate-code
 # pylint: disable=unused-variable
 
+
 import pandas as pd
+from matplotlib import pyplot as plt
+
 # import numpy as np
 
 # Read in the data
@@ -29,10 +32,35 @@ def tally_categories():
     tally = {}
     for index, row in df.iterrows():
         if row['Category'] in tally:
-            tally[row['Category']] += row['Amount']
+            #category already exists
+            if row['Transaction Type'] == 'credit':
+                #credit
+                tally[row['Category']] += row['Amount']
+            else:
+                #debit
+                tally[row['Category']] -= row['Amount']
         else:
+            #create new category
             tally[row['Category']] = row['Amount']
+    del tally['Transfer']
     return tally
+
+def plot_histogram():
+    x = []
+    y = []
+    for key, value in tally_categories().items():
+        x.append(key)
+        y.append(value)
+    fig = plt.figure(figsize = (20, 10))
+    plt.bar(x, y, color ='maroon', width = 1.0)
+    plt.xticks(rotation=30, ha='right')
+    
+    plt.xlabel("Categories")
+    plt.ylabel("Amount spent")
+    plt.title("Amount spent on each category")
+    plt.savefig("Amount Spent.png")
 
 # print out the account balance
 print(calc_account_balance())
+
+plot_histogram()
