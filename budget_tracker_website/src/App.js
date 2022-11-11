e316170ac4a96001e0b986a102825c743ef94afa
Armani <Armanichien2021@gmail.com>
2022-11-11 16:32:35 -0600

    pageSwitch

--- budget_tracker_website/src/App.js
+++ budget_tracker_website/src/App.js
@@ -10,17 +10,23 @@ import {About} from './Components/About';
 
 
 function App() {
-  // let history = useHistory();
+
+  // use useHistory
+  // const history = useHistory();
 
   return (
     <Router>
       <div className='bg'>
           <div className='nav'>
               <h1> Budget Tracker App </h1>
-              {/* Create a button of class "navLi" that switches to the '/about' route */}
-              <button class="navLi">Home</button>
+
+              <a class="navLi" href="/home">Home</a>
+              <a class="navLi" href="/about">About</a>
+              <a class="navLi" href="/">Projects</a>
+          
+              {/* <button class="navLi">Home</button>
               <button class="navLi">About</button>
-              <button class="navLi">Projects</button>
+              <button class="navLi">Projects</button> */}
           </div>
           <div className='content'>
             <Switch>
