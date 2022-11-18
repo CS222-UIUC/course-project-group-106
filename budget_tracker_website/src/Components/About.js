04ca0d0ee2c698e570c54937cf87702f1e1f9a1e
Cayla <caylajr2@illinois.edu>
2022-11-18 15:21:07 -0600

    updated text on home page and about page, images not showing up properly on home page

--- budget_tracker_website/src/Components/About.js
+++ budget_tracker_website/src/Components/About.js
@@ -1,6 +1,16 @@
+import "./About.css";
 function About() {
     return (
-        <h1>About</h1>
+    <div>
+        <h1>About Us</h1>
+        <p>We are a group of Computer Science students at the University of Illinois. For our CS 222 project, we decided to create an app that helps you track your spending habits.</p>
+        <p>Contact Info:<br></br>
+        Armani Chien: armanic2@illinois.edu<br></br>
+        Becky Blake: ryblake2@illinois.edu<br></br>
+        Cayla Risinger: caylajr2@illinois.edu<br></br>
+        Palak Kotwani: kotwani3@illinois.edu</p>
+    </div>
+    
     );
 }
 
--- budget_tracker_website/src/Components/Home.js
+++ budget_tracker_website/src/Components/Home.js
@@ -1,6 +1,14 @@
 function Home() {
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <p>This app will help track your budget -- how much you spend and on what. To use our app, head to the projects tab!</p>
+            <img src="budget_tracker_website\Spending Pie Chart.png" alt="Spending Pie Chart"></img>
+            <caption>% Spending/Category Example Pie Chart</caption>
+            <p><br></br></p>
+            <img src="budget_tracker_website\Spending Over Time.png" alt="Spending Over Time Graph"></img>
+            <caption>Spending/Time Example Graph</caption>
+        </div>
     );
 }
 
